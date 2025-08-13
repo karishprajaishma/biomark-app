a3caa285638933eb84a99c9c635066780e4d02d6
Apurav <apurav0711@gmail.com>
2025-08-13 13:28:20 +0530

    modified:   server/fast_server.py

--- server/fast_server.py
+++ server/fast_server.py
@@ -679,7 +679,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # use Render's PORT if provided
 
     uvicorn.run(
-        "main:app", 
+        app,
         host="0.0.0.0", 
         port=port, 
         reload=False,
